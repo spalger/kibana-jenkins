83e3c841e9336c38b3c875d482850e2bdead89a4
spalger <email@spalger.com>
2018-11-13 12:02:26 -0800

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+import exampleRoute from './server/routes/example';
+
+export default function (kibana) {
+  return new kibana.Plugin({
+    require: ['elasticsearch'],
+    name: 'jenkins',
+    uiExports: {
+      app: {
+        title: 'Jenkins',
+        description: 'Jenkins View in Kibana',
+        main: 'plugins/jenkins/app',
+      },
+      styleSheetPaths: require('path').resolve(__dirname, 'public/app.scss'),
+    },
+
+    config(Joi) {
+      return Joi.object({
+        enabled: Joi.boolean().default(true),
+      }).default();
+    },
+
+    init(server, options) { // eslint-disable-line no-unused-vars
+      // Add server routes and initialize the plugin here
+      exampleRoute(server);
+    }
+  });
+}
